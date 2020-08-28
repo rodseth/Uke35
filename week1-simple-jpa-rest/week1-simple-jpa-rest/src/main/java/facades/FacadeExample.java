4ed169767c50634eecbdfb12bc11cb83305274b0
rodseth <59828269+rodseth@users.noreply.github.com>
2020-08-28 18:10:00 +0200

    Add files via upload

---
+++ week1-simple-jpa-rest/week1-simple-jpa-rest/src/main/java/facades/FacadeExample.java
@@ -0,0 +1,36 @@
+package facades;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+/**
+ *
+ * Rename Class to a relevant name Add add relevant facade methods
+ */
+public class FacadeExample {
+
+    private static FacadeExample instance;
+    private static EntityManagerFactory emf;
+    
+    //Private Constructor to ensure Singleton
+    private FacadeExample() {}
+    
+    
+    /**
+     * 
+     * @param _emf
+     * @return an instance of this facade class.
+     */
+    public static FacadeExample getFacadeExample(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new FacadeExample();
+        }
+        return instance;
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+}
